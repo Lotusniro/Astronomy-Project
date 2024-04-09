0947d5cbdac21df233f8a932e446504d148525ed
Lotusniro <145142238+Lotusniro@users.noreply.github.com>
2024-04-09 22:10:04 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -36,53 +36,53 @@ $("#fetch-image-button").on("click",function(){
 //https://api.le-systeme-solaire.net/rest/bodies/{id} 
 
 
-function fetchPlanetData(planet) {
-    var url = `https://api.le-systeme-solaire.net/rest/bodies/${planet}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            displayPlanetData(data);
-        });
-}   
+// function fetchPlanetData(planet) {
+//     var url = `https://api.le-systeme-solaire.net/rest/bodies/${planet}`;
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//             displayPlanetData(data);
+//         });
+// }   
 
 
 
-$('#fetch-planet-button').on('click', function() {
-    var input = $('#planet-input').val();
-    fetchPlanetData(input);
+// $('#fetch-planet-button').on('click', function() {
+//     var input = $('#planet-input').val();
+//     fetchPlanetData(input);
 
-    // Check if the button already exists
-    if (!$("#planety button").text().includes(input)) {
-        localStorage.setItem("planet", input);
-        let btn = $("<button>").text(input);
-        btn.addClass("btn btn-outline-secondary btn-block");
-        $("#planety").append(btn);
+//     // Check if the button already exists
+//     if (!$("#planety button").text().includes(input)) {
+//         localStorage.setItem("planet", input);
+//         let btn = $("<button>").text(input);
+//         btn.addClass("btn btn-outline-secondary btn-block");
+//         $("#planety").append(btn);
 
-        // Event handler for the new button
-        btn.on("click", function() {
-            $('#planet-input').val(input);
-            fetchPlanetData(input);
-        });
-    }
-});
+//         // Event handler for the new button
+//         btn.on("click", function() {
+//             $('#planet-input').val(input);
+//             fetchPlanetData(input);
+//         });
+//     }
+// });
 
 
 
-function displayPlanetData(data) {
-    var planetDataDiv = $('#planet-data');
-    planetDataDiv.html(`
-        <h1>${data.englishName}</h1>
-        <p>Gravity: ${data.gravity}</p>
-        <p>Density: ${data.density}</p>
-        <p>Mass: ${data.mass.massValue} ${data.mass.massExponent}</p>
-        <p>Escape Velocity: ${data.escape}</p>
-        <p>Equatorial Radius: ${data.equaRadius}</p>
-        <p>Polar Radius: ${data.polarRadius}</p>
-        <p>Mean Radius: ${data.meanRadius}</p>
-        <p>Sideral Rotation: ${data.sideralRotation}</p>
-        <p>Sideral Orbit: ${data.sideralOrbit}</p>
-        <p>Aphelion: ${data.aphelion}</p>
-        <!-- Add more fields as needed -->
-    `);
-    }   
+// function displayPlanetData(data) {
+//     var planetDataDiv = $('#planet-data');
+//     planetDataDiv.html(`
+//         <h1>${data.englishName}</h1>
+//         <p>Gravity: ${data.gravity}</p>
+//         <p>Density: ${data.density}</p>
+//         <p>Mass: ${data.mass.massValue} ${data.mass.massExponent}</p>
+//         <p>Escape Velocity: ${data.escape}</p>
+//         <p>Equatorial Radius: ${data.equaRadius}</p>
+//         <p>Polar Radius: ${data.polarRadius}</p>
+//         <p>Mean Radius: ${data.meanRadius}</p>
+//         <p>Sideral Rotation: ${data.sideralRotation}</p>
+//         <p>Sideral Orbit: ${data.sideralOrbit}</p>
+//         <p>Aphelion: ${data.aphelion}</p>
+//         <!-- Add more fields as needed -->
+//     `);
+//     }   
